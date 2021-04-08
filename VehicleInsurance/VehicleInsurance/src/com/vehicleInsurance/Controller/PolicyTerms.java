34249ee7017d1fc49c2915ae9dd404b0bea8cc16
sumanthreddy-29 <61323947+sumanthreddy-29@users.noreply.github.com>
2021-04-08 15:36:00 +0530

    Add files via upload

---
+++ VehicleInsurance/VehicleInsurance/src/com/vehicleInsurance/Controller/PolicyTerms.java
@@ -0,0 +1,25 @@
+package com.vehicleInsurance.Controller;
+
+public class PolicyTerms {
+	public void policyTerms() {
+		System.out.println("=========================================*********************================================================");
+		
+		System.out.println("Please read the below terms and conditions carefully: ");
+		System.out.println("1. Insurance is done only for bike and car, we're trying to cross this barrier in future by incorporating for other vehicles.");
+		System.out.println("2. There are two kinds of memberships:");
+		System.out.println("    -> Premium Membership");
+		System.out.println("    -> Regular Membership");
+		System.out.println("3. For premium membership 12% of your vehicle amount is charged for one year and you have to pay using monthly installments");
+		System.out.println("4. For regular membership 12% of your vehicle amount is charged for one year and you have to pay using monthly installments");
+		System.out.println("5. Insurance can't be claimed when your vehicle is damaged or lost by natural calamities");
+		System.out.println("6. The benifits on taking premium or regular membership is disclosed and you can get the details with the help of customer care");
+		System.out.println("********************************");
+		System.out.println("P.no: 1800-200-100");
+		System.out.println("Email: customercarexyz@gmail.com");
+		System.out.println("********************************");
+		
+		//System.out.println("**************************Thankyou*******************************************");
+		
+	}
+
+}
